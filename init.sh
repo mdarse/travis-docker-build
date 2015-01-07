eb3c86c5a346a95eec696fcbadcf8ee4bd39fc34
Mathieu Darse <mdarse@jolicode.com>
2015-01-07 11:26:38 +0100

    Prevent panic on init script end failure

--- init.sh
+++ init.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 set -e
 
+save_and_shutdown() {
+  # save built for host result
+  # force clean shutdown
+  halt -f
+}
+# make sure we shut down cleanly
+trap save_and_shutdown EXIT SIGINT SIGTERM
+
+
 cd $WORKDIR
 pwd
 
