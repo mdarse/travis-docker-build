725c724be08485f6cf54a337975f268b48e17648
Mathieu Darse <mdarse@jolicode.com>
2015-01-08 18:10:07 +0100

    Wait for docker

---
+++ wait_for_docker.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo -n "Waiting for docker to start"
+until curl http://localhost:2375/info
+# while netstat -lnt | awk '$4 ~ /:2375$/ {exit 1}'
+do
+    echo -n "."
+    sleep 1
+done
+echo
