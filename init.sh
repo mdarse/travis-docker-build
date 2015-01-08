9a2765bd02202e1322f4fa3058ae95d06bcc5e03
Mathieu Darse <mdarse@jolicode.com>
2015-01-08 16:50:05 +0100

    Export DOCKER_HOST

--- init.sh
+++ init.sh
@@ -51,7 +51,8 @@ ip route add default via 10.0.2.2
 # start docker daemon
 echo "Starting docker daemon..."
 exec docker -d -H tcp://0.0.0.0:2375 &
-echo "$ docker version -H tcp://0.0.0.0:2375"
-docker version -H tcp://0.0.0.0:2375
+export DOCKER_HOST="tcp://0.0.0.0:2375"
+echo "$ docker version"
+docker version
 # exec docker -d -H tcp://0.0.0.0:2375 > "$STDOUT" 2> "$STDERR"
 # echo "Docker daemon exited."
